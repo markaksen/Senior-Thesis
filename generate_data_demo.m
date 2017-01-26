3afbd0b2a7e3f3aacb5adcdc796aa0050cf79b56
markaksen <maksen@princeton.edu>
2017-01-26 17:13:22 -0500

    fixed some function callls

--- generate_data_demo.m
+++ generate_data_demo.m
@@ -1,12 +1,12 @@
 %file = 'C:/Mark/College/Senior Year/Senior Thesis/code/EMD-6454.map';
 %[data] = ReadMRC(file);
-[x y z]=ndgrid(-3:3);
- data=(x.^2+(y*1.5).^2+z.^2)<2^2
-[ALM_m, C_m] = spherical_harm_coeff(data);
-[Cl] = recon_spherical_harm_coeff(ALM_m);
-save('data.mat');
-%load('data.mat')
-%K = 50; maxphi = 50; maxtheta = 20;
-%[phi_tilde] = recon_fourier_transform(Cl, K, maxphi, maxtheta);
-%[phi] = recon_3D_structure(phi_tilde);
-%WriteMRC(phi,1,'EMD-6454_2.mrc');
+%[x y z]=ndgrid(-3:3);
+ %data=(x.^2+(y*1.5).^2+z.^2)<2^2
+%[ALM_m, C_m] = spherical_harm_coeff(data);
+%[Cl] = recon_spherical_harm_coeff(ALM_m);
+%save('data3.mat');
+load('data3.mat')
+K = 50; maxphi = 50; maxtheta = 20;
+[phi_tilde] = recon_fourier_transform(Cl, K, maxphi, maxtheta);
+[phi] = recon_3D_structure(phi_tilde);
+WriteMRC(phi,1,'EMD-6454_2.mrc');
--- recon_fourier_transform.m
+++ recon_fourier_transform.m
@@ -4,17 +4,20 @@
 function [phi_tilde] = recon_fourier_transform(Cl,K, maxphi, maxtheta)
 
 phi_interval = 2*pi/(maxphi-1);
-theta_interal = 2*pi/(maxtheta-1);
+theta_interval = 2*pi/(maxtheta-1);
 
 phi_tilde = zeros(K, maxphi, maxtheta);
 for k = 0:K
     for phi = [0:phi_interval:2*pi]
         for theta = [0:theta_interval:2*pi]
             sum = 0;
-            for l = 0:K
-                for m = -l:l
-                    if m < 0
-                        sum = sum+Cl[-(m+1)].*Ylm(theta, phi);
-                    else 
-                        sum =  sum+Cl[l+m].*Ylm(theta,phi);
+            for l = 1:K
+                for m = 0:(2*l)
+                        sum = sum+Cl(l+m).*ylm(l,m, theta, phi);
+                end
+            end
             phi_tilde(k,phi,theta) = sum;
+        end
+    end
+end
+end
