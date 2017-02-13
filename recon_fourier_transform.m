b75402bcd25257d7b39685868d4c312df0d492df
markaksen <maksen@princeton.edu>
2017-02-13 14:00:26 -0500

    Add files via upload

--- recon_fourier_transform.m
+++ recon_fourier_transform.m
@@ -1,24 +1,31 @@
 %-------------------------------------------------------
     % Step 3: This function computes reconstructed fourier transform
     %-------------------------------------------------------
-function [phi_tilde] = recon_fourier_transform(Cl,K, maxphi, maxtheta)
+function [phi_hat] = recon_fourier_transform(ALM,maxphi, maxtheta)
 
+[cnt,max, nonzero] = count(ALM);
+K = max;
 phi_interval = 2*pi/(maxphi-1);
 theta_interval = 2*pi/(maxtheta-1); 
-theta = [0:theta_interval:2*pi]
-phi_tilde = zeros(K, maxphi, maxtheta);
-for k = 0:K
-    for phi = [0:phi_interval:2*pi]
-        sum = 0;
-        for l = 1:K
-             for m = 0:(2*l)
-                  [l,m, phi]
-                  size_Cl = size(Cl{l+m})
-                  size_ylm = size(ylm(l,m,theta,phi))
-                  sum = sum+Cl{l+m}.*ylm(l,m, theta, phi);
-             end
+phi_hat = zeros(K, maxphi, maxtheta);
+for k = 1:K
+    for phi_index = [0:maxphi-1]
+        phi = phi_index*phi_interval;
+        for theta_index = [0:maxtheta-1]
+            theta = theta_index*theta_interval;
+            sum = 0;
+            for l = 1:nonzero
+                 for m = 1:(2*l-1)
+                    x = ALM{l};
+                    sz = size(x);
+                    if (sz(1)>= k)
+                         y = x(k,m);
+                        sum = sum+(y.*ylm(l,m-l-1, theta, phi));
+                    end
+                 end
+            end
+            phi_hat(k,phi_index+1,theta_index+1) = sum;
         end
-        phi_tilde(k,phi) = sum;
     end
 end
 end
