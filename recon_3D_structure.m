95a964179c8ca7d770f9f53881d9171e592d8013
markaksen <maksen@princeton.edu>
2017-03-20 10:21:57 -0400

    Delete recon_3D_structure.m

--- recon_3D_structure.m
+++
@@ -1,5 +0,0 @@
-%-------------------------------------------------------
-    % Step 4: This function computes reconstructed 3D structure
-    %-------------------------------------------------------
-function [phi] = recon_3D_structure(phi_tilde)
-phi = fftshift( ifftn( ifftshift(phi_tilde))); % need to add parameters for the inverse transforms
